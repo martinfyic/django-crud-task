31c2094e76d3d9d4cf2a5666dbc5f0d82075c0b8
Martin Ferreira <martin.f.yic@gmail.com>
2023-05-06 22:03:09 -0300

    feat: :sparkles: add task views.py

---
+++ tasks/views.py
@@ -0,0 +1,68 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login, logout, authenticate
+from django.db import IntegrityError
+from .forms import TaskForm
+
+# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def signup(request):
+    if request.method == 'GET':
+         return render(request, 'signup.html', { 'form': UserCreationForm })
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(username=request.POST['username'], password=request.POST['password1'])
+                user.save()
+                login(request, user)
+                return redirect('tasks')
+            except IntegrityError:
+                return render(request, 'signup.html', { 
+                    'form': UserCreationForm, 
+                    'error': 'El usuario ya existe'
+                })
+        else:
+            return render(request, 'signup.html', { 
+                'form': UserCreationForm, 
+                'error': 'Password no coincide' 
+            })
+
+def signout(request):
+    logout(request)
+    return redirect('home')
+
+def signin(request):
+   if request.method == 'GET':
+        return render(request, 'signin.html', {
+        'form': AuthenticationForm
+        })
+   else:
+       user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+       
+       if user is None:
+           return render(request, 'signin.html', {
+            'form': AuthenticationForm,
+            'error': 'Usuario o contraseña incorrecto'
+           })
+       else:
+            login(request, user)
+            return redirect('tasks')
+
+def tasks(request):
+    return render(request, 'tasks.html')
+
+def create_task(request):
+    if request.method == 'GET':
+        return render(request, 'create_task.html', {
+        'form': TaskForm
+    })
+    else:
+        print(request.POST)
+        return render(request, 'create_task.html', {
+        'form': TaskForm
+    })
+
+    
