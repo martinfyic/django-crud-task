d1a33fe9c8610af7b3f66492c377dcc527122139
Martin Ferreira <martin.f.yic@gmail.com>
2023-05-07 23:41:54 -0300

    perf: :sparkles: protect urls with @login_required

--- tasks/views.py
+++ tasks/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.decorators import login_required
 from django.db import IntegrityError
+from django.utils import timezone
 from .forms import TaskForm
 from .models import Task
 
@@ -20,7 +22,7 @@ def signup(request):
                 user = User.objects.create_user(username=request.POST['username'], password=request.POST['password1'])
                 user.save()
                 login(request, user)
-                return redirect('tasks')
+                return redirect('tasks_pending')
             except IntegrityError:
                 return render(request, 'signup.html', { 
                     'form': UserCreationForm, 
@@ -33,6 +35,7 @@ def signup(request):
             })
 
 
+@login_required
 def signout(request):
     logout(request)
     return redirect('home')
@@ -53,16 +56,26 @@ def signin(request):
            })
        else:
             login(request, user)
-            return redirect('tasks')
+            return redirect('tasks_pending')
 
 
-def tasks(request):
+@login_required
+def tasks_pending(request):
     tasks = Task.objects.filter(user=request.user, datecompleted__isnull=True)
-    return render(request, 'tasks.html', {
+    return render(request, 'tasks_pending.html', {
         'tasks': tasks
     })
 
 
+@login_required
+def tasks_complete(request):
+    tasks = Task.objects.filter(user=request.user, datecompleted__isnull=False).order_by('datecompleted')
+    return render(request, 'tasks_complete.html', {
+        'tasks': tasks
+    })
+
+
+@login_required
 def create_task(request):
     if request.method == 'GET':
         return render(request, 'create_task.html', {
@@ -74,7 +87,7 @@ def create_task(request):
             new_task = form.save(commit=False)
             new_task.user = request.user
             new_task.save()
-            return redirect('tasks')
+            return redirect('tasks_pending')
         except ValueError:
              return render(request, 'create_task.html', {
                 'form': TaskForm,
@@ -82,8 +95,41 @@ def create_task(request):
             })
 
 
+@login_required
 def task_detail(request, id):
-    task = get_object_or_404(Task, pk=id)
-    return render(request, 'task_detail.html', {
-        'task': task
-    })
+    if request.method == 'GET':
+        task = get_object_or_404(Task, pk=id, user=request.user)
+        form = TaskForm(instance=task)
+        return render(request, 'task_detail.html', {
+            'task': task,
+            'form': form
+        })
+    else:
+        try:
+            task = get_object_or_404(Task, pk=id, user=request.user)
+            form = TaskForm(request.POST, instance=task)
+            form.save()
+            return redirect('tasks_pending')
+        except ValueError:
+            return render(request, 'task_detail.html', {
+            'task': task,
+            'form': form,
+            'error': 'Los datos no son validos'
+        })
+
+
+@login_required
+def task_complete(request, id):
+    task = get_object_or_404(Task, pk=id, user=request.user)
+    if request.method == 'POST':
+        task.datecompleted = timezone.now()
+        task.save()
+        return redirect('tasks_pending')
+    
+
+@login_required
+def task_delete(request, id):
+    task = get_object_or_404(Task, pk=id, user=request.user)
+    if request.method == 'POST':
+        task.delete()
+        return redirect('tasks_pending')
